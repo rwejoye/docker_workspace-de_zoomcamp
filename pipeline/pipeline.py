5ca39687e8077dddbe875e6974e8d495372b4a19
Richmond Wejoye <wejoyerichmond@gmail.com>
2026-06-04 14:41:12 +0000

    Project ongoing: Pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys, pandas as pd
+print("arguments", sys.argv)
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"A":[1,2], "B":[3,4]})
+df['month'] = month
+
+print(df.head())
+df.to_parquet(f"output_{month}.parquet")
+print(f'Hello pipeline, the month is {month}')
